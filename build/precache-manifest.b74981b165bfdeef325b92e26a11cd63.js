43e6beea7c15cb8ee1169db5c3497401481c9efa
laixiaoyin <1012551253@qq.com>
2019-11-24 17:56:08 +0800

    add build package

---
+++ build/precache-manifest.b74981b165bfdeef325b92e26a11cd63.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789ed86e569399d87099b7e45ad36e5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74835b10b6f0a23b4474",
+    "url": "/static/css/2.69ad0371.chunk.css"
+  },
+  {
+    "revision": "d45ea19d3f2880a3ab43",
+    "url": "/static/css/main.db0044ad.chunk.css"
+  },
+  {
+    "revision": "74835b10b6f0a23b4474",
+    "url": "/static/js/2.c06a37e1.chunk.js"
+  },
+  {
+    "revision": "d45ea19d3f2880a3ab43",
+    "url": "/static/js/main.1ffdb423.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "277e1882892294f3b4d14c672c84fdd6",
+    "url": "/static/media/erc20_logo.277e1882.svg"
+  },
+  {
+    "revision": "c85884b42dd0fa75dfcb0b24bdd1745b",
+    "url": "/static/media/erc721_logo.c85884b4.svg"
+  },
+  {
+    "revision": "6d3ab03c52315cca1e95b8e2e6489426",
+    "url": "/static/media/install_metamask.6d3ab03c.svg"
+  },
+  {
+    "revision": "bac7491467df990edc64591ffc639d62",
+    "url": "/static/media/red_exclamation_sign.bac74914.svg"
+  }
+]);
